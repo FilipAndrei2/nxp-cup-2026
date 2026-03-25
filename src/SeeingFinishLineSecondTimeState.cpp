86e7b100f7983281fecd092f027e4526dabda59f
Filip Andrei-Robert <andrei-robert.filip@student.tuiasi.ro>
2026-03-25 18:09:32 +0200

    refactor: SeeingFinishLineSecondTimeState:filterInfoVectors: extracted logic into separated functions

--- src/SeeingFinishLineSecondTimeState.cpp
+++ src/SeeingFinishLineSecondTimeState.cpp
@@ -10,52 +10,74 @@ namespace ls {
 const ls::DrivingCommandDTO SeeingFinishLineSecondTimeState::computeCommand(
     const ls::SensorDataDTO &sensorData) {
   std::array<FVector2, 5> infoVectors;
-  size_t numberInfoVectors = 0;
-  for (auto &v : *sensorData.vectors) {
+
+  uint8_t numberInfoVectors =
+      filterInfoVectors(sensorData.vectors, infoVectors);
+
+  angle_t angle;
+  speed_t speed;
+
+  computeSpeedAndAngle(infoVectors, numberInfoVectors, angle, speed);
+
+  speed = scaleSpeedByCubeProximity(sensorData.cubeProximity, speed);
+
+  return DrivingCommandDTO{
+      .angle = angle, .speed = speed, .shouldStop = false}; // todo: finish
+}
+
+/**
+ * @returns number of info vectors
+ */
+static uint8_t filterInfoVectors(const std::vector<FVector2> &inVectors,
+                                 std::array<FVector2, 5> &outInfoVectors) {
+  uint8_t numberInfoVectors = 0;
+  for (auto &v : inVectors) {
     if (numberInfoVectors >= 5) {
       break;
     }
     if (!Vectors::isFinishLine(v) && v != Vectors::ZERO) {
-      infoVectors[numberInfoVectors] = v;
+      outInfoVectors[numberInfoVectors] = v;
       numberInfoVectors++;
     }
   }
+  return numberInfoVectors;
+}
 
-  angle_t angle;
-  speed_t speed;
-  switch (numberInfoVectors) {
+static void computeSpeedAndAngle(const std::array<FVector2, 5> &inVectors,
+                                 const uint8_t inNumberInfoVectors,
+                                 angle_t &outAngle, speed_t &outSpeed) {
+  switch (inNumberInfoVectors) {
   case 0: {
     // Fara vizibilitate -> ii dam orbeste inainte
-    angle = 0.0f;
-    speed = Speed::WAITING_CUBE_SPEED;
+    outAngle = 0.0f;
+    outSpeed = Speed::WAITING_CUBE_SPEED;
     break;
   }
   case 1: {
     // Vedem 1 vector -> calculam unghiul dupa el
-    angle = Vector2<float>::AngleBetween(infoVectors[0], Vectors::NORTH);
-    speed = Speed::scale(Speed::WAITING_CUBE_SPEED, angle);
+    outAngle = Vector2<float>::AngleBetween(inVectors[0], Vectors::NORTH);
+    outSpeed = Speed::scale(Speed::WAITING_CUBE_SPEED, outAngle);
     break;
   }
 
   case 2:
   default: {
     // Vedem 2 sau mai multi -> ii luam pe primi 2, calculam avg si unghi/speed
-    FVector2 medi = Vector2<float>::Avg(infoVectors[0], infoVectors[1]);
-    angle = Vector2<float>::AngleBetween(medi, Vectors::NORTH);
-    speed = Speed::scale(Speed::WAITING_CUBE_SPEED, angle);
+    FVector2 medi = Vector2<float>::Avg(inVectors[0], inVectors[1]);
+    outAngle = Vector2<float>::AngleBetween(medi, Vectors::NORTH);
+    outSpeed = Speed::scale(Speed::WAITING_CUBE_SPEED, outAngle);
     break;
   }
   }
-
-  // Urmeaza sa scalam viteza in functie de senzorul de proxi
-  if (sensorData.cubeProximity > 0) {
-    speed = speed * (sensorData.cubeProximity / 100);
-  }
-
-  return DrivingCommandDTO{
-      .angle = angle, .speed = speed, .shouldStop = false}; // todo: finish
 }
 
+static speed_t scaleSpeedByCubeProximity(const proximity_t proximity,
+                                         const speed_t maxSpeed) {
+  if (proximity > 0) {
+    return maxSpeed * (proximity / 100);
+  }
+  return maxSpeed;
+}
 void SeeingFinishLineSecondTimeState::updateNextState(
     const ls::SensorDataDTO &sensorData, ATrackStateContext &ctx) const {
   // schimba stateul daca nu mai vezi fin, sau daca incepem sa percepem cubul
