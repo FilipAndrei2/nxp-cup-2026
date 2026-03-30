0fa9467ec26bf535f6a990f17d1f33469e3e704b
Filip Andrei-Robert <filandrei4@protonmail.com>
2026-03-30 17:15:33 +0300

    fix: unsaved speed scale bug fix

--- include/params/speed.hpp
+++ include/params/speed.hpp
@@ -18,7 +18,7 @@ public:
   }
 
   static speed_t scale(const speed_t maxSpeed, const proximity_t cubeProxi) {
-    return maxSpeed * (cubeProxi / 100);
+    return maxSpeed * cubeProxi;
   }
 };
 } // namespace ls
